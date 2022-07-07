4f64e36d7f3840789508d79d88978abd92c36f89
Ragimi19 <103253894+Ragimi19@users.noreply.github.com>
2022-07-07 20:09:05 +0530

    Add files via upload

---
+++ MatterJSBoilerPlate-main/sketch.js
@@ -0,0 +1,51 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+    groundObj=new ground(width/2,670,width,20);
+	leftSide=new ground(1100,600,20,120);
+	rightSide=new ground(1350,600,20,120);
+	var ball_options={
+		isStatic:false,
+		restitution:0.3,
+		friction:0,
+		density:1.2
+	}
+    
+
+
+	ball = Bodies.circle(260,100,radius/2,ball_options);
+	World.add(world,ball);
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  ellipse(ball.position.x,ball.position.y,radius,radius);
+  groundObj.sjow();
+  leftSide.show();
+  rightSide.show();
+  drawSprites();
+ 
+}
+
+
+
